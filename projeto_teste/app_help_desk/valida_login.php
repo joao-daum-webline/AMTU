003007cdf18743d86c63f4cc7b27b110fa1e7881
joao-daum-webline <134417600+joao-daum-webline@users.noreply.github.com>
2023-05-23 14:39:16 -0300

    Add files via upload

---
+++ projeto_teste/app_help_desk/valida_login.php
@@ -0,0 +1,45 @@
+<?php
+
+
+	//variavel que verifica se a autenticacao foi realizada
+	
+
+
+	//usuarios do sistema
+	$usuarios_app = array(
+		array('id' => 1, 'email' => 'adm@teste.com.br', 'senha' => '1234', 'perfil_id' => 1),
+		array('id' => 2, 'email' => 'user@teste.com.br', 'senha' => '1234', 'perfil_id' => 1),
+		array('id' => 3, 'email' => 'jose@teste.com.br', 'senha' => '1234', 'perfil_id' => 2),
+		array('id' => 4, 'email' => 'maria@teste.com.br', 'senha' => '1234', 'perfil_id' => 2),
+	);
+
+	/*
+	echo '<pre>';
+	print_r($usuarios_app);
+	echo '</pre>';
+	*/
+
+
+
+
+
+	/*
+	print_r($_GET);
+
+	echo '<br />';
+
+	echo $_GET['email'];
+	echo '<br />';
+	echo $_GET['senha'];
+	*/
+
+	/*
+	print_r($_POST);
+
+	echo '<br />';
+
+	echo $_POST['email'];
+	echo '<br />';
+	echo $_POST['senha'];
+	*/
+?>
